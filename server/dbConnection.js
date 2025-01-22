e73bce3edb9a27f9ddb164563ae91c669084adb0
farnaz <farnaz.towhidi@gmail.com>
2025-01-22 13:43:54 -0500

    update

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,11 +1,23 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv';
 
-const dataBase = mysql.createConnection ( {
-  host: 'localhost',
-  user: 'root',
-  password: 'Ntw29tir*',
-  database: 'QAnswer'
-})
+dotenv.config();  // Load .env variables
+
+// const dataBase = mysql.createConnection ( {
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'Ntw29tir*',
+//   database: 'QAnswer'
+// })
+
+
+const db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,      // 'roundhouse.proxy.rlwy.net'
+  user: process.env.MYSQL_USER,      // 'root'
+  password: process.env.MYSQL_PASSWORD, // 'IBPfvSDukookFeBZPSEaSfBaynoMTErd'
+  database: process.env.MYSQL_DATABASE, // 'QAnswer'
+  port: process.env.MYSQL_PORT || 14325 // Use the port provided in the string (or default to 14325)
+});
 
 dataBase.connect ((err)=> {
   if (err) {
